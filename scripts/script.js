ec425a35e1203020cdc0b81da39c23ac23b909d6
EmreBahar0912 <e.bahar906@gmail.com>
2025-10-01 15:44:14 +0200

    Header + hamburger menu #8 #11
    Header opmaak gemaakt + hamburger menu toegevoegd in de header.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
-// JS
+const hamButton = document.querySelector(".hamburger-icon");
+const hamSluitButton = document.querySelector(".hamburger-menu button");
+
+const hamMenu = document.querySelector(".hamburger-menu");
+
+hamButton.addEventListener("click", () => {
+  hamButton.classList.toggle("active");
+  hamMenu.classList.add("is-open");
+});
+
+hamSluitButton.addEventListener("click", () => {
+  hamButton.classList.toggle("is-open");
+  hamMenu.classList.remove("is-open");
+});
+
+if (hamMenu.classList.contains("is-open")) {
+  document.body.style.opacity = "0.5";
+} else {
+  document.body.style.opacity = "1";
+}
